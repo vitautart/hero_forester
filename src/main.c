a45c6bbb44886cdd86e8e0ab146d01939cab1cb6
vitautart <vitartach@gmail.com>
2020-11-28 00:44:44 +0200

    Added tiling layout

--- src/main.c
+++ src/main.c
@@ -3,6 +3,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
+typedef struct tiling_layout_t
+{
+    int *data;
+    int width;
+    int height;
+} tiling_layout_t;
+
+int get_tiling_layout_cell(tiling_layout_t* layout, int x, int y)
+{
+    return layout->data[x + y * layout->width];
+}
+
+tiling_layout_t create_tiling_layout(int width, int height)
+{
+    tiling_layout_t l = {.width = width, .height = height};
+    int size_in_bytes = width * height * sizeof(int);
+    l.data = malloc(size_in_bytes);
+    memset(l.data, 0, size_in_bytes);
+    return l;
+}
+
+
+
+void free_tiling_layout(tiling_layout_t layout)
+{
+    free(layout.data);
+}
 
 float norm_rand()
 {
@@ -16,6 +45,7 @@ void load_resources (Texture2D** textures, int* texture_count)
 
     (*textures)[0] = LoadTexture("assets/uncle.png");
     (*textures)[1] = LoadTexture("assets/tree.png");
+    (*textures)[2] = LoadTexture("assets/grass.png");
 }
 
 void unload_resources (Texture2D* textures, int texture_count)
@@ -23,6 +53,21 @@ void unload_resources (Texture2D* textures, int texture_count)
     for (int i = 0; i < texture_count; i++)
         UnloadTexture(textures[i]);
     free(textures);
+}
+
+void draw_tiled_background(Texture2D* textures, tiling_layout_t layout)
+{
+    for (int x = 0; x < layout.width; x++)
+    {
+        for (int y = 0; y < layout.height; y++)
+        {
+            int id = get_tiling_layout_cell(&layout, x, y);
+            int x_pos = x * 32;
+            int y_pos = y * 32;
+            DrawTexture(textures[id], x_pos, y_pos, WHITE);
+        }
+    }
+
 }
 
 int main(void)
@@ -49,15 +94,19 @@ int main(void)
         .zoom = 3.0f
     };
 
+    tiling_layout_t layout = create_tiling_layout(50, 50);
+
+
     InitWindow(screen_w, screen_h, "HERO FORESTER");
 
     Texture2D* textures = NULL;
     int texture_count;
 
     load_resources(&textures, &texture_count);
+    Texture2D tiling_textures [] = {textures[2]};
+
 
     SetTargetFPS(60);
-    printf("dfgdfg");
     while(!WindowShouldClose())
     {
         if (IsKeyDown(KEY_LEFT))    player_pos.x -= 1.0f;
@@ -68,7 +117,9 @@ int main(void)
         BeginDrawing();
         ClearBackground((Color){ .r = 0, .g = 100, .b = 0, .a = 255 });
         {
+            camera.target = player_pos;
             BeginMode2D(camera);
+            draw_tiled_background(tiling_textures, layout);
             DrawTexture(textures[0], (int)player_pos.x, (int)player_pos.y, WHITE);
             for (int i = 0; i < entity_count; i++)
                 DrawTexture(textures[1], (int)positions[i].x, (int)positions[i].y, WHITE);
@@ -78,6 +129,7 @@ int main(void)
         EndDrawing();
     }
 
+    free_tiling_layout(layout);
     unload_resources(textures, texture_count);
     free(positions);
     CloseWindow();
